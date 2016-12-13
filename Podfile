42658d324a71d18734b7d43c03a2058bd85e6d54
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-12-13 20:49:34 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+platform :ios, '9.0'
+swift_version = '3.0'
+
+target 'SaladaSample' do
+  use_frameworks!
+  pod 'Firebase'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Database'
+
+  pod 'SVProgressHUD', :git => 'https://github.com/SVProgressHUD/SVProgressHUD.git'
+  pod 'SDWebImage'
+
+  post_install do |installer|
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['SWIFT_VERSION'] = '3.0'
+      end
+    end
+  end
+end
---
+++ SaladaSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaladaSample
+//
+//  Created by 酒井文也 on 2016/12/13.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
